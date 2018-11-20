ca168d6e41f952b2027a3d6aa27a18b87b20758b
paul <paul@acme.org>
2018-11-20 10:27:57 +0000

    secrets

--- src/main/java/catalogue/CatalogueService.java
+++ src/main/java/catalogue/CatalogueService.java
@@ -1,12 +1,20 @@
 package catalogue;
 
 import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class CatalogueService {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(CatalogueService.class);
+
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
+
+        logger.debug("Database credentials: username = {}, password = {}", System.getenv("DB_USERNAME"), System.getenv("DB_PASSWORD"));
+        
 
         app.get("/ping", ctx -> ctx.result("Hello World "));
 
