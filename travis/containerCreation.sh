68e6386312c996aab43dd37da01aba607dfd1d2f
josejulioramos@outlook.com <josejulioramos@outlook.com>
2020-02-13 09:46:25 +0000

    Adds container creation script

---
+++ travis/containerCreation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# fail fast settings from https://dougrichardson.org/2018/08/03/fail-fast-bash-scripting.html
+set -euov pipefail
+
+# Create a Docker image and tag it as 'travis_<build number>'
+buildTag=travis_${TRAVIS_BRANCH}_$TRAVIS_BUILD_NUMBER
+
+docker build -t eoepca/template-service .
+docker tag eoepca/template-service $DOCKER_USERNAME/template-service:$buildTag
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push $DOCKER_USERNAME/template-service:$buildTag   # defaults to docker hub
