1c81b4e3871c25edaac786bbe17deede3258f2b9
josejulioramos@outlook.com <josejulioramos@outlook.com>
2020-02-13 12:06:43 +0000

    Trying with more sleep time

--- travis/acceptanceTest.sh
+++ travis/acceptanceTest.sh
@@ -4,5 +4,5 @@
 set -euov pipefail
 
 docker run -d --rm -p 8080:7000 --name template-svc $DOCKER_USERNAME/template-service:travis_${TRAVIS_BRANCH}_$TRAVIS_BUILD_NUMBER
-sleep 15  # wait for startup
-curl -s http://localhost:8080/search  # trivial smoke test
+sleep 30  # wait for startup
+curl -s http://localhost:8080/search | jq '.' # trivial smoke test
