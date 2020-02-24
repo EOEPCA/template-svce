0b284ef3d11c4b6f8b87ea59788426de43b83f8a
josejulioramos@outlook.com <josejulioramos@outlook.com>
2020-02-24 11:10:42 +0000

    Testing

--- travis/acceptanceTest.sh
+++ travis/acceptanceTest.sh
@@ -8,5 +8,5 @@ TRAVIS_BRANCH="${TRAVIS_BRANCH:-local}"
 TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-NaN}"
 
 docker run -d --rm -p 8080:7000 --name template-svc ${DOCKER_USERNAME}/template-service:travis_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}
-sleep 30  # wait for startup
-curl -s http://localhost:8080/search # trivial smoke test
+sleep 15  # wait for startup
+curl -vs http://localhost:8080/search # trivial smoke test
