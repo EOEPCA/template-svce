18a0ccb85f033e4561833932ddd00a2755c78249
josejulioramos@outlook.com <josejulioramos@outlook.com>
2020-02-24 11:00:30 +0000

    Fixes acceptance test

--- travis/acceptanceTest.sh
+++ travis/acceptanceTest.sh
@@ -3,6 +3,10 @@
 # fail fast settings from https://dougrichardson.org/2018/08/03/fail-fast-bash-scripting.html
 set -euov pipefail
 
-docker run -d --rm -p 8080:7000 --name template-svc $DOCKER_USERNAME/template-service:travis_${TRAVIS_BRANCH}_$TRAVIS_BUILD_NUMBER
+# Check presence of environment variables
+TRAVIS_BRANCH="${TRAVIS_BRANCH:-local}"
+TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-NaN}"
+
+docker run -d --rm -p 8080:7000 --name template-svc ${DOCKER_USERNAME}/template-service:travis_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}
 sleep 30  # wait for startup
-curl -s http://localhost:8080/search | jq '.' # trivial smoke test
+curl -s http://localhost:8080/search # trivial smoke test
