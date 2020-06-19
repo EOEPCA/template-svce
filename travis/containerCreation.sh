40f5d49710659466f6c21fb761e28d16f08357a4
Richard Conway <richard.conway@telespazio.com>
2020-06-19 18:14:03 +0100

    Version release pipeline steps triggered by git tag (GitHub release) (#14)
    Trigger release/publish steps based upon git tags rather than master branch

--- travis/containerCreation.sh
+++ travis/containerCreation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # fail fast settings from https://dougrichardson.org/2018/08/03/fail-fast-bash-scripting.html
-set -euov pipefail
+set -eov pipefail
 
 # obtain current repository name
 REPO_LOCAL_PATH=`git rev-parse --show-toplevel`
@@ -18,6 +18,10 @@ buildTag=travis_$TRAVIS_BUILD_NUMBER # We use a temporary build number for taggi
 docker build -t eoepca/${REPO_NAME} .
 docker tag eoepca/${REPO_NAME} eoepca/${REPO_NAME}:$buildTag # Tags container in EOEPCA repository with buildTag
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-docker push eoepca/${REPO_NAME}:$buildTag   # defaults to docker hub EOEPCA repository
+if [ -n "${DOCKER_USERNAME}" -a -n "${DOCKER_PASSWORD}" ]
+then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push eoepca/${REPO_NAME}:$buildTag   # defaults to docker hub EOEPCA repository
+else
+  echo "WARNING: No credentials - Cannot push to docker hub"
+fi
--- travis/release.sh
+++ travis/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # fail fast settings from https://dougrichardson.org/2018/08/03/fail-fast-bash-scripting.html
-set -euov pipefail
+set -eov pipefail
 
 # Check presence of environment variables
 TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-0}"
@@ -13,13 +13,18 @@ REPO_NAME=`basename $REPO_LOCAL_PATH`
 # Create a Docker image and tag it as 'travis_<build number>'
 buildTag=travis_$TRAVIS_BUILD_NUMBER # We use a temporary build number for tagging, since this is a transient artefact
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker pull eoepca/${REPO_NAME}:${buildTag}  # have to pull locally in order to tag as a release
+if [ -n "${DOCKER_USERNAME}" -a -n "${DOCKER_PASSWORD}" ]
+then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker pull eoepca/${REPO_NAME}:${buildTag}  # have to pull locally in order to tag as a release
 
-# Tag and push as a Release following the SemVer approach, e.g. 0.1.1-Alpha
-docker tag eoepca/${REPO_NAME}:${buildTag} eoepca/${REPO_NAME}:release_${TRAVIS_TAG} # This recovers the GitHub release/tag number
-docker push eoepca/${REPO_NAME}:release_${TRAVIS_TAG}
+  # Tag and push as a Release following the SemVer approach, e.g. 0.1.1-Alpha
+  docker tag eoepca/${REPO_NAME}:${buildTag} eoepca/${REPO_NAME}:${TRAVIS_TAG} # This recovers the GitHub release/tag number
+  docker push eoepca/${REPO_NAME}:${TRAVIS_TAG}
 
-# Tag and push as `latest`
-docker tag eoepca/${REPO_NAME}:${buildTag} eoepca/${REPO_NAME}:latest
-docker push eoepca/${REPO_NAME}:latest
+  # Tag and push as `latest`
+  docker tag eoepca/${REPO_NAME}:${buildTag} eoepca/${REPO_NAME}:latest
+  docker push eoepca/${REPO_NAME}:latest
+else
+  echo "WARNING: No credentials - Cannot push to docker hub"
+fi
